4fbf20279babcd6d1ca2bac838b816017a9f3867
RakeshSuvvari <rakeshsuvvari@Rakeshs-Laptop.local>
2025-04-22 10:09:59 -0500

    logs

--- cbp.c
+++ cbp.c
@@ -26,30 +26,45 @@ int main() {
     log_event("CBP started");
 
     FramePart part;
+    int first_sender = -1;
     while (1) {
         Recvfrom(sockfd, &part, sizeof(part), 0, NULL, NULL);
 
         char msg[256];
-        snprintf(msg, sizeof(msg), "Received part %d of frame %d from Station %d to Station %d",
-            part.part, part.frame_number, part.src_station, part.dst_station);
+        snprintf(msg, sizeof(msg), "Receive part %d of frame %d from Station %d, to Station %d",
+                 part.part, part.frame_number, part.src_station, part.dst_station);
         log_event(msg);
 
         if (buffer_state == 0 && part.part == 1) {
             memcpy(&buffer, &part, sizeof(part));
             buffer_state = 1;
+            first_sender = part.src_station;
         } else if (buffer_state == 1 && part.part == 2 &&
                    part.src_station == buffer.src_station &&
                    part.frame_number == buffer.frame_number) {
-            buffer_state = 0;
-            snprintf(msg, sizeof(msg), "Delivered frame %d to Station %d",
-                     part.frame_number, part.dst_station);
+
+            snprintf(msg, sizeof(msg), "Transfer part 1 of frame %d from Station %d, to Station %d",
+                     buffer.frame_number, buffer.src_station, buffer.dst_station);
             log_event(msg);
+
+            snprintf(msg, sizeof(msg), "Transfer part 2 of frame %d from Station %d, to Station %d",
+                     part.frame_number, part.src_station, part.dst_station);
+            log_event(msg);
+
+            buffer_state = 0;
+            first_sender = -1;
         } else {
-            snprintf(msg, sizeof(msg), "Collision detected on frame %d from Station %d",
-                     part.frame_number, part.src_station);
+            snprintf(msg, sizeof(msg), "Inform Station %d, Station %d, a collision",
+                     first_sender, part.src_station);
             log_event(msg);
+
+            snprintf(msg, sizeof(msg), "A collision informed, wait for 1 time slot");
+            log_event(msg);
+
             buffer_state = 0;
+            first_sender = -1;
         }
     }
+
     return 0;
 }
--- station.c
+++ station.c
@@ -40,25 +40,30 @@ int main(int argc, char *argv[]) {
         FramePart p1 = {id, dst, frame, 1, "Data1"};
         FramePart p2 = {id, dst, frame, 2, "Data2"};
 
-        log_event(id, "Send part 1");
+        char msg[128];
+        snprintf(msg, sizeof(msg), "Send part 1 of frame %d to Station %d", frame, dst);
+        log_event(id, msg);
         Sendto(sockfd, &p1, sizeof(p1), 0, (struct sockaddr*)&addr, sizeof(addr));
-
         wait_slot(1);
 
         if (rand() % 5 == 0) {
-            log_event(id, "Collision detected, retrying");
-            int backoff = rand() % (1 << (collision_count < 10 ? collision_count : 10));
-            wait_slot(backoff);
+            int slots = rand() % (1 << (collision_count < 10 ? collision_count : 10));
+            snprintf(msg, sizeof(msg), "A collision informed, wait for %d time slot", slots);
+            log_event(id, msg);
+            wait_slot(slots);
             collision_count++;
             continue;
         }
 
-        log_event(id, "Send part 2");
+        snprintf(msg, sizeof(msg), "Send part 2 of frame %d to Station %d", frame, dst);
+        log_event(id, msg);
         Sendto(sockfd, &p2, sizeof(p2), 0, (struct sockaddr*)&addr, sizeof(addr));
+
         frame++;
         collision_count = 0;
         wait_slot(1);
     }
+
     fclose(f);
     return 0;
 }
